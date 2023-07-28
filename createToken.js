dbfdefe907c78a28ee1801d4abe110bda2d00205
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-28 15:33:12 +0300

    added createToken middleware

---
+++ createToken.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+
+const secretKey = 'ombasaJoshua'
+
+function createToken(user) {
+    const payload = {
+        id: user.id,
+        email: user.email
+    }
+
+    return jwt.sign(payload, secretKey, {expiresIn : '1h'}) // Token expires in 1 hour
+
+}
+
+module.exports = createToken
