b6ba9fe7dd74d282af4870d8720533b82e466249
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-30 12:35:34 +0300

    moved authenticateToken middleware to an external module

---
+++ authenticateToken.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const secretKey = process.env.JWT_SECRET
+
+module.exports = {
+    authenticateToken: authenticateToken
+}
+
+function authenticateToken(req, res, next) {
+    const token = req.header('Authorization');
+
+    if (!token) {
+        return res.status(401).json({ message: 'Authentication token is missing.' });
+    } else if (token) {
+        jwt.verify(token, secretKey, (err, user) => {
+            if (err) {
+                return res.status(403).json({ message: 'Invalid token.' });
+            }
+
+            next();
+        });
+    }
+}
